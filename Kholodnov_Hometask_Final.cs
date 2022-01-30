f25750bc9d9954cb7da32d3e47f8fb65853bcaf3
Evgeny Kholodnov <evgenykholodnov@yandex.ru>
2022-01-31 01:33:12 +0300

    Introduced myself

--- Kholodnov_Hometask_Final.cs
+++ Kholodnov_Hometask_Final.cs
@@ -1,1 +1,13 @@
-﻿
+﻿void Introduction()
+{
+    Console.WriteLine();
+    Console.WriteLine("Aloha, my dear friend!");
+    Console.WriteLine("Let me introduce myself first:");
+    Console.WriteLine("My name is Evgeny Kholodnov.");
+    Console.WriteLine("You can contact me via evgenykholodnov@yandex.ru");
+    Console.WriteLine("Or via @evgeny_kholodnov in Telegram and Instagram");
+    Console.WriteLine();
+    Console.WriteLine("Please, take a look at my solution of the assigned task below:");
+    Console.WriteLine();
+}
+Introduction();
